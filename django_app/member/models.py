2ef89deec103ecb7c716ee418c8237229e5cead8
ym4199 <ym4199@gmail.com>
2017-08-05 17:09:30 +0900

    User model 생성

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=15)
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'nickname'
+    REQUIRED_FIELDS = ['email']
