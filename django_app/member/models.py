6d394b6fe41deaff0e5495ac352415cc6d7383e0
ym4199 <ym4199@gmail.com>
2017-08-05 17:12:03 +0900

    User relation 생성

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -9,3 +9,16 @@ class User(AbstractUser):
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'nickname'
     REQUIRED_FIELDS = ['email']
+
+
+class Relation(models.Model):
+    user1 = models.ForeignKey(User, related_name='request_user')
+    user2 = models.ForeignKey(User, related_name='response_user')
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (
+            (
+                'user1', 'user2'
+            ),
+        )
